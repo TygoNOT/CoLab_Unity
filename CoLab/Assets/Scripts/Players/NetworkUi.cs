fbaf2b0dc92a35141b4be1278811112977161a91
Artvlx <arthur.prokopenko@gmail.com>
2025-05-12 00:29:00 +0200

    unlucky

--- CoLab/Assets/Scripts/Players/NetworkUi.cs
+++ CoLab/Assets/Scripts/Players/NetworkUi.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 using Unity.Netcode;
 using UnityEngine.UI;
 using TMPro;
+using Unity.Netcode.Transports.UTP;
 
 public class NetworkUi : NetworkBehaviour
 {
     [SerializeField] private TextMeshProUGUI playersCountText;
     [SerializeField] private GameObject ButtonHolder;
     [SerializeField] private GameObject Players;
+    [SerializeField] private TMP_InputField ipInputField;
 
     private NetworkVariable<int> playersNumber = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone);
+    void Start()
+    {
+        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnected;
+    }
+
+    private void OnClientConnected(ulong clientId)
+    {
+        Debug.Log($"Client connected: {clientId}");
+    }
+
+    private void OnClientDisconnected(ulong clientId)
+    {
+        Debug.Log($"Client disconnected: {clientId}");
+    }
     public void HostButtonClick()
     {
         NetworkManager.Singleton.StartHost();
@@ -20,8 +37,16 @@ public class NetworkUi : NetworkBehaviour
     }
     public void ClientButtonClick()
     {
-        string hostIp = "26.116.245.55";
-        NetworkManager.Singleton.GetComponent<Unity.Netcode.Transports.UTP.UnityTransport>().SetConnectionData(hostIp, 7777);
+        string ipAddress = ipInputField.text.Trim();
+
+        if (string.IsNullOrEmpty(ipAddress))
+        {
+            ipAddress = "127.0.0.1";
+        }
+
+        UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        transport.SetConnectionData(ipAddress, (ushort)7777);
+
         NetworkManager.Singleton.StartClient();
         ButtonHolder.SetActive(false);
         Players.SetActive(true);
