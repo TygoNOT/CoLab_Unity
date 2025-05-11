808a88f8c6184c84bcac06e21aabea0b3f7e7948
Artvlx <arthur.prokopenko@gmail.com>
2025-05-11 23:06:44 +0200

    dfhdf

--- CoLab/Assets/Scripts/Players/NetworkUi.cs
+++ CoLab/Assets/Scripts/Players/NetworkUi.cs
@@ -20,6 +20,8 @@ public class NetworkUi : NetworkBehaviour
     }
     public void ClientButtonClick()
     {
+        string hostIp = "26.116.245.55";
+        NetworkManager.Singleton.GetComponent<Unity.Netcode.Transports.UTP.UnityTransport>().SetConnectionData(hostIp, 7777);
         NetworkManager.Singleton.StartClient();
         ButtonHolder.SetActive(false);
         Players.SetActive(true);
